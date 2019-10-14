223b3a0845f9aa280e89c7f5c33ba5d532d61bb4
JayaKrishna <namburu1995@gmail.com>
2019-10-14 14:40:40 +0530

    Configuring gatsby to use dash case in css modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,12 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ loaders, actions }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.less$/,
+          use: [loaders.css({ camelCase: false })],
+        },
+      ],
+    },
+  })
+}
